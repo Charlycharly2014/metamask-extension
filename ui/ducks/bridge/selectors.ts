0b2055146e066287f53e1ce00749ab825d2ff696
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-06-26 17:59:26 -0700

    chore: add bridge chain selectors

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -3,10 +3,16 @@ import { ProviderConfig } from '@metamask/network-controller';
 import { getProviderConfig } from '../metamask/metamask';
 import { getIsBridgeEnabled } from '../../selectors';
 import { BridgeState } from './bridge';
+import { ALLOWED_BRIDGE_CHAIN_IDS } from '../../../shared/constants/bridge';
 
-export const getFromChain = getProviderConfig;
+export const getFromChain = (state: any): ProviderConfig =>
+  getProviderConfig(state);
 export const getToChain = (state: { bridge: BridgeState }): ProviderConfig =>
   state.bridge.toChain;
+// TODO read from feature flags and return ProviderConfig/RPCDefinition
+export const getFromChains = (state: any) => ALLOWED_BRIDGE_CHAIN_IDS;
+// TODO read from feature flags and return ProviderConfig/RPCDefinition
+export const getToChains = (state: any) => ALLOWED_BRIDGE_CHAIN_IDS;
 
 export const getIsBridgeTx = createSelector(
   getFromChain,
